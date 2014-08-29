a3e1efc165691a0f44a07e9a9ba1cb52ebe6ba67
Mossie93 <rmossakowski@gmail.com>
2014-08-29 15:50:04 +0200

    First build

---
+++ CMakeFiles/ContrastBrightness.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ContrastBrightness.dir/main.cpp.o"
+  "ContrastBrightness.pdb"
+  "ContrastBrightness"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ContrastBrightness.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cv.h>
+#include <highgui.h>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+	Mat inputImg, outputImg;
+	double alpha;	//controls contrast
+	int beta;		//controls brightness
+
+	//checking if all parameters are present
+	if(argc != 3)
+	{
+		cout << "There should be exactly  parameters!\n";
+		return -1;
+	}
+
+	//loading images
+	inputImg = imread(argv[1]);
+	if(!inputImg.data)
+	{
+		cout << "Error: input file opening failure!\n";
+	}
+	outputImg = Mat::zeros(inputImg.size(), inputImg.type());
+
+	//transofrmating image
+	for(int y = 0; y < inputImg.rows; y++)
+	{
+		for(int x = 0; x < inputImg.cols; x++)
+		{
+			for(int c = 0; c < 3; c++)	//c stands for channel
+			{
+				outputImg.at<Vec3b>(y,x)[c] = saturate_cast<uchar>( alpha * ( inputImg.at<Vec3b>(y,x)[c] ) + beta );
+			}
+		}
+	}
+
+	//saving output image
+	imwrite(argv[2], outputImg);
+
+	//displaying images
+	namedWindow("Input image", CV_WINDOW_AUTOSIZE);
+	namedWindow("Output image", CV_WINDOW_AUTOSIZE);
+	imshow("Input image", inputImg);
+	imshow("Output image", outputImg);
+
+	//waiting for signal to close application
+	waitKey(0);
+	return 0;
+}
